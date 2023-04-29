854c4afe2605e78112016f87686d81509ea88685
Faraz <farazatgithub@gmail.com>
2023-04-29 13:08:20 +0500

    validation for email in use modal

--- model/User.js
+++ model/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -8,7 +9,14 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    validate:{
+        validator:function(input){
+         return validator.isEmail(input)
+        },
+        message:"please provide valid email"
+    },
     required: [true, "plz provide email"],
+    unique:true
   },
   password: {
     type: String,
