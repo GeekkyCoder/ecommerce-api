a91f1a06a4e21cd9ccaa66dc2f3793cee62f377e
Faraz <farazatgithub@gmail.com>
2023-05-13 10:10:27 +0500

    permissions for updating review controler

--- controllers/review/review.controller.js
+++ controllers/review/review.controller.js
@@ -42,28 +42,40 @@ const getSingleReview = async (req, res) => {
     return res.status(400).json({ msg: "no review for this product" });
   }
 
-//   const hasPermissions = checkPermissions(req.user, review.user);
+  //   const hasPermissions = checkPermissions(req.user, review.user);
 
-//   if (!hasPermissions) {
-//     return res
-//       .status(403)
-//       .json({ msg: "you are not authorized to access this resource" });
-//   }
+  //   if (!hasPermissions) {
+  //     return res
+  //       .status(403)
+  //       .json({ msg: "you are not authorized to access this resource" });
+  //   }
 
   res.status(200).json({ review });
 };
 
 const updateReview = async (req, res) => {
-  const {id:reviewId} = req.params 
+  const { id: reviewId } = req.params;
 
-  const review = await Review.findOneAndUpdate({_id:reviewId},req.body,{new:true,runValidators:true})
+  const reviewExist = await Review.findOne({ _id: reviewId });
 
-  if(!review) {
-    return res.status(400).json({msg:`no review with ${reviewId} found`})
+  const hasPersmissions = checkPermissions(req.user, reviewExist.user);
+
+  if (!hasPersmissions) {
+    return res
+      .status(403)
+      .json({ msg: "not authorized to access this resource" });
   }
 
-  res.status(200).json({review})
-  
+  const review = await Review.findOneAndUpdate({ _id: reviewId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!review) {
+    return res.status(400).json({ msg: `no review with ${reviewId} found` });
+  }
+
+  res.status(200).json({ review });
 };
 
 const deleteReview = async (req, res) => {
