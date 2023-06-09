a37c5667f7d10579e5fb2fb2947dc5ea5336b11d
Faraz <farazatgithub@gmail.com>
2023-06-09 12:04:02 +0500

    create order controller

--- controllers/orders/orders.controllers.js
+++ controllers/orders/orders.controllers.js
@@ -1,7 +1,68 @@
+const Order = require("../../model/Order");
 const Orders = require("../../model/Order");
+const Product = require("../../model/Product");
 
-const createOrder = (req, res) => {
-  res.send("create orders");
+const fakeStripeApi = async ({ amount, currency }) => {
+  const clientSecret = "someRandomText";
+  return { clientSecret, amount };
+};
+
+const createOrder = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    return res.status(400).json({ msg: "no ordered items" });
+  }
+
+  if (!shippingFee || !tax) {
+    return res.status(400).json({ msg: "shippingFee and Tax not provided" });
+  }
+
+  let orderItems = [];
+  let subTotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+
+    if (!dbProduct) {
+      return res
+        .status(400)
+        .json({ msg: `product with id ${item.product} does not exist` });
+    }
+
+    const { name, image, price, _id } = dbProduct;
+
+    const orderItem = {
+      name,
+      image,
+      price,
+      amount: item.amount,
+      product: _id,
+    };
+
+    orderItems = [...ordertems, orderItem];
+    subTotal += item.amount * price;
+  }
+
+  const total = subTotal + tax + shippingFee;
+
+  //fake stripe api
+  const paymentIntent = await fakeStripeApi({
+    amount: total,
+    currency: "usd",
+  });
+
+  const order = await Order.create({
+    orderItems,
+    tax,
+    shippingFee,
+    subtotal,
+    total,
+    user: req.user.userId,
+    clientSecret: paymentIntent.clientSecret,
+  });
+
+  res.status(201).json({ order, clientSecret: order.clientSecret });
 };
 
 const getAllOrders = async (req, res) => {
@@ -21,9 +82,9 @@ const updateOrder = (req, res) => {
   res.send("update Order");
 };
 
-const deleteOrder = (req,res) => {
-  res.send("delete order")
-}
+const deleteOrder = (req, res) => {
+  res.send("delete order");
+};
 
 module.exports = {
   createOrder,
@@ -31,5 +92,5 @@ module.exports = {
   getSingleOrder,
   getCurrentUserOrders,
   updateOrder,
-  deleteOrder
+  deleteOrder,
 };
--- routes/order/order.routes.js
+++ routes/order/order.routes.js
@@ -1,13 +1,21 @@
-const { getAllOrders, getSingleOrder, createOrder, updateOrder, deleteOrder, getCurrentUserOrders } = require('../../controllers/orders/orders.controllers')
-const authMiddleware = require('../../middleware/authentication')
+const {
+  getAllOrders,
+  getSingleOrder,
+  createOrder,
+  updateOrder,
+  deleteOrder,
+  getCurrentUserOrders,
+} = require("../../controllers/orders/orders.controllers");
+const authMiddleware = require("../../middleware/authentication");
+const authorize = require("../../middleware/authorize");
 
-const orderRouter = require('express').Router()
+const orderRouter = require("express").Router();
 
-orderRouter.get('/',authMiddleware, getAllOrders)
-orderRouter.post("/", authMiddleware, createOrder)
-orderRouter.get("/currentUser", authMiddleware, getCurrentUserOrders)
-orderRouter.get("/:id", authMiddleware,getSingleOrder)
-orderRouter.patch("/:id", authMiddleware, updateOrder)
-orderRouter.delete("/:id", authMiddleware, deleteOrder)
+orderRouter.get("/", authMiddleware, authorize("admin", "owner"), getAllOrders);
+orderRouter.post("/", authMiddleware, createOrder);
+orderRouter.get("/currentUser", authMiddleware, getCurrentUserOrders);
+orderRouter.get("/:id", authMiddleware, getSingleOrder);
+orderRouter.patch("/:id", authMiddleware, updateOrder);
+orderRouter.delete("/:id", authMiddleware, deleteOrder);
 
-module.exports = orderRouter
+module.exports = orderRouter;
